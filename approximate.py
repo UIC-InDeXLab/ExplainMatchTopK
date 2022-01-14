22b7d5fe6305a9cea1e22eaa85797a3be7ef1227
IanSwift <idealistictechnophilia@gmail.com>
2022-01-14 10:45:30 -0600

    Start work on approximate

---
+++ approximate.py
@@ -0,0 +1,77 @@
+import random
+import heapq
+
+def preProcess(vectors, weights):
+    results = {}
+    weightedVectors = []
+    for vector in vectors:
+        weightedVector = []
+        for i in range(len(weights)):
+            weightedVector.append(vector[i] * weights[i])
+        weightedVectors.append(weightedVector)
+    results['Weighted Vectors'] = weightedVectors
+    attributeLists = []
+    for a in range(len(weights)):
+        attributeList = [(weightedVectors[x][a], x) for x in range(len(weightedVectors))]
+        attributeList.sort(reverse=True)
+        attributeLists.append(attributeList)
+    results['Attribute Lists'] = attributeLists
+    return results
+
+def getTopK(preprocessed, attributes, k):
+    if (len(attributes) == 0):
+        return []
+    threshold = float('inf')
+    seenVectors = set()
+    vectorValues = {}
+    position = 0
+    while len([vector for vector in seenVectors if vectorValues[vector] >= threshold]) < k:
+        currentValues = [attributeList[position] for attributeList in [preprocessed['Attribute Lists'][attribute] for attribute in attributes]]
+        threshold = sum([x[0] for x in currentValues])
+        for vector in [x[1] for x in currentValues]:
+            if vector not in seenVectors:
+                currentVector = preprocessed['Weighted Vectors'][vector]
+                vectorValues[vector] = sum([currentVector[x] for x in attributes])
+                seenVectors.add(vector)
+        position = position + 1
+    resultVectors = [(vectorValues[vector] * -1, vector) for vector in seenVectors]
+    heapq.heapify(resultVectors)
+    topK = []
+    for x in range(k):
+        topK.append(heapq.heappop(resultVectors)[1])
+    return topK
+    
+
+
+def approximateShapleyInTopK(preprocessed, d, m, k, j):
+    scores = [0 for x in range(d)]
+    attributes = [x for x in range(d)]
+    for mi in range(m):
+        permutation = attributes.copy()
+        random.shuffle(attributes)
+        for ai in range(len(permutation)):
+            prev = getTopK(preprocessed, permutation[:ai], k)
+            curr = getTopK(preprocessed, permutation[:ai+1], k)
+            currScore = 1 if j in curr else 0
+            prevScore = 0 if ai == 0 else (1 if j in prev else 0)
+            scores[permutation[ai]] = scores[permutation[ai]] + (currScore - prevScore)/m
+    return scores
+
+def approximateShapleyNotInTopK(preprocessed, d, m, k, j):
+    scores = [0 for x in range(d)]
+    attributes = [x for x in range(d)]
+    for mi in range(m):
+        permutation = attributes.copy()
+        random.shuffle(attributes)
+        for ai in range(len(permutation)):
+            prev = getTopK(preprocessed, permutation[:ai], k)
+            curr = getTopK(preprocessed, permutation[:ai+1], k)
+            currScore = 1 if j not in curr else 0
+            prevScore = 0 if ai == 0 else (1 if j not in prev else 0)
+            scores[permutation[ai]] = scores[permutation[ai]] + (currScore - prevScore)/m
+    return scores
+
+vectors = [[5,3,1],[2,4,4],[3,1,2],[4,1,3],[1,2,5]]
+weights = [80,90,4]
+print(approximateShapleyInTopK(preProcess(vectors, weights), 3, 10000, 2, 0))
+print(approximateShapleyNotInTopK(preProcess(vectors, weights), 3, 10000, 2, 0))
--- brute-force.py
+++ brute-force.py
@@ -1,4 +1,5 @@
 import math
+import heapq
 
 def ComputeShapleyInTopK(vectors, weights, k, j):
     scores = [0 for x in range(len(weights))]
@@ -14,15 +15,81 @@ def ComputeShapleyInTopK(vectors, weights, k, j):
             curr = [x[:] for x in prev[:]]
             for vector in range(len(vectors)):
                 curr[vector][0] = curr[vector][0] + vectors[vector][c] * weights[c]
-            print(prev)
-            print(curr)
-            if j in [x[1] for x  in sorted(curr, key=lambda e: e[0], reverse=True)[:k]] and j not in [x[1] for x in sorted(prev, key=lambda e: e[0], reverse=True)[:k]]:
-                scores[c] = scores[c] + 1/(2**(len(weights)-1))
+            prevTuples = [(x[0]*-1, x[1]) for x in prev]
+            currTuples = [(x[0]*-1, x[1]) for x in curr]
+            heapq.heapify(prevTuples)
+            heapq.heapify(currTuples)
+            topKPrev = []
+            topKCurr = []
+            for x in range(k):
+                topKPrev.append(heapq.heappop(prevTuples)[1])
+                topKCurr.append(heapq.heappop(currTuples)[1])
+            currScore = 1 if j in topKCurr else 0
+            prevScore = 0 if prev[j][0] == 0 else (1 if j in topKPrev else 0)
+            scores[c] = scores[c] + (currScore - prevScore)/(2**(len(weights)-1))
     return scores
                     
             
+def ComputeShapleyNotInTopK(vectors, weights, k, j):
+    scores = [0 for x in range(len(weights))]
+    for c in range(len(weights)): # c - dimensions       
+        for a in range(2**len(weights)): # a - various attribute combinations
+            if math.floor(a/(2**c))%(2) == 0:
+                continue
+            prev = [[0, x] for x in range(len(vectors))]
+            for b in range(len(weights)):
+                if math.floor(a/(2**b))%(2) == 0:
+                    for vector in range(len(vectors)):
+                        prev[vector][0] = prev[vector][0] + vectors[vector][b] * weights[b]
+            curr = [x[:] for x in prev[:]]
+            for vector in range(len(vectors)):
+                curr[vector][0] = curr[vector][0] + vectors[vector][c] * weights[c]
+            prevTuples = [(x[0]*-1, x[1]) for x in prev]
+            currTuples = [(x[0]*-1, x[1]) for x in curr]
+            heapq.heapify(prevTuples)
+            heapq.heapify(currTuples)
+            topKPrev = []
+            topKCurr = []
+            for x in range(k):
+                topKPrev.append(heapq.heappop(prevTuples)[1])
+                topKCurr.append(heapq.heappop(currTuples)[1])
+            currScore = 1 if j not in topKCurr else 0
+            prevScore = 0 if prev[j][0] == 0 else (1 if j not in topKPrev else 0)
+            scores[c] = scores[c] + (currScore - prevScore)/(2**(len(weights)-1))
+    return scores        
                 
+   
+def ComputeShapleyTopKLookLikeThis(vectors, weights, k):
+    scores = [0 for x in range(len(weights))]
+    for c in range(len(weights)): # c - dimensions       
+        for a in range(2**len(weights)): # a - various attribute combinations
+            if math.floor(a/(2**c))%(2) == 0:
+                continue
+            prev = [[0, x] for x in range(len(vectors))]
+            for b in range(len(weights)):
+                if math.floor(a/(2**b))%(2) == 0:
+                    for vector in range(len(vectors)):
+                        prev[vector][0] = prev[vector][0] + vectors[vector][b] * weights[b]
+            curr = [x[:] for x in prev[:]]
+            for vector in range(len(vectors)):
+                curr[vector][0] = curr[vector][0] + vectors[vector][c] * weights[c]
+            prevTuples = [(x[0]*-1, x[1]) for x in prev]
+            currTuples = [(x[0]*-1, x[1]) for x in curr]
+            heapq.heapify(prevTuples)
+            heapq.heapify(currTuples)
+            topKPrev = []
+            topKCurr = []
+            for x in range(k):
+                topKPrev.append(heapq.heappop(prevTuples)[1])
+                topKCurr.append(heapq.heappop(currTuples)[1])
+            setCurrTopK = set(topKCurr)
+            setPrevTopK = set(topKPrev)
+            IoU = 1-len(setCurrTopK.intersection(setPrevTopK))/len(setCurrTopK.union(setPrevTopK)) # Jaccard similarity
+            scores[c] = scores[c] + (IoU)/(2**(len(weights)-1)) 
+    return scores                  
     
 vectors = [[5,3,1],[2,4,4],[3,1,2],[4,1,3],[1,2,5]]
 weights = [80,90,4]
-print(ComputeShapleyInTopK(vectors, weights, 2, 0))
+print(ComputeShapleyInTopK(vectors,weights, 2, 0))
+print(ComputeShapleyNotInTopK(vectors,weights, 2, 0))
+print(ComputeShapleyTopKLookLikeThis(vectors,weights, 2))
