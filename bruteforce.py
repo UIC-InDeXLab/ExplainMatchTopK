25bd69198e2018110c384e449cec169762496cad
IanSwift <idealistictechnophilia@gmail.com>
2022-03-25 23:46:07 -0500

    Add hashing to bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -2,7 +2,6 @@ import math
 import heapq
 import itertools
 import topk
-import pdb
 import utility
 
 def ComputeShapleyInTopK(vectors, evaluationFunction, k, j, algorithm='GeneralPurpose'):
@@ -48,80 +47,132 @@ def ComputeShapleyInTopK(vectors, evaluationFunction, k, j, algorithm='GeneralPu
             
 def ComputeShapleyNotInTopK(vectors, evaluationFunction, k, j, algorithm='GeneralPurpose'):
     scores = [0 for x in range(len(vectors[0]))]
+    previousSeen = {}
+    dFactorial = math.factorial(len(vectors[0]))
     if algorithm == 'Threshold':
         attributeLists = topk.preProcess(vectors, evaluationFunction)
     for permutation in itertools.permutations(range(len(vectors[0]))):
+        currHash = 0
         for position in range(len(vectors[0])):
+            prevHash = currHash
+            currHash = currHash | (1 << permutation[position])
             if algorithm == 'Threshold':
-                prevTopK = topk.computeTopKThreshold(vectors, attributeLists, evaluationFunction, permutation[:position], k)
-                currTopK = topk.computeTopKThreshold(vectors, attributeLists, evaluationFunction, permutation[:position+1], k)
-                prevScore = 0 if position == 0 else (1 if not j in prevTopK else 0)
-                currScore = 1 if not j in currTopK else 0
+                if prevHash not in previousSeen:
+                    prevTopK = topk.computeTopKThreshold(vectors, attributeLists, evaluationFunction, permutation[:position], k)
+                    prevScore = 0 if position == 0 else (1 if not j in prevTopK else 0)
+                    previousSeen[prevHash] = prevScore
+                else:
+                    prevScore = previousSeen[prevHash]
+                if currHash not in previousSeen:
+                    currTopK = topk.computeTopKThreshold(vectors, attributeLists, evaluationFunction, permutation[:position+1], k)
+                    currScore = 1 if not j in currTopK else 0
+                    previousSeen[currHash] = currScore
+                else:
+                    currScore = previousSeen[currHash]
             else:
-                prevTuples = topk.generateTuples(vectors, evaluationFunction, permutation, position)
-                currTuples = topk.generateTuples(vectors, evaluationFunction, permutation, position+1)
-                prevScore = 0 if position == 0 else (1 if not topk.computeInTopK(prevTuples, k, j) else 0)
-                currScore = 1 if not topk.computeInTopK(currTuples, k, j) else 0
-            scores[permutation[position]] = scores[permutation[position]] + (currScore - prevScore)/math.factorial(len(vectors[0]))
+                if prevHash not in previousSeen:
+                    prevTuples = topk.generateTuples(vectors, evaluationFunction, permutation, position)
+                    prevScore = 0 if position == 0 else (1 if not topk.computeInTopK(prevTuples, k, j) else 0)
+                    previousSeen[prevHash] = prevScore
+                else:
+                    prevScore = previousSeen[prevHash]
+                if currHash not in previousSeen:
+                    currTuples = topk.generateTuples(vectors, evaluationFunction, permutation, position+1)
+                    currScore = 1 if not topk.computeInTopK(currTuples, k, j) else 0
+                    previousSeen[currHash] = currScore
+                else:
+                    currScore = previousSeen[currHash]
+            scores[permutation[position]] = scores[permutation[position]] + (currScore - prevScore)/dFactorial
     return scores        
                 
    
 def ComputeShapleyTopKLookLikeThis(vectors, evaluationFunction, k, algorithm='GeneralPurpose'):
-    scores = [0 for x in range(len(vectors[0]))]
+    scores = [0 for x in range(len(weights))]
+    dFactorial = math.factorial(len(vectors[0]))
+    previousSeen = {}
     if algorithm == 'Threshold':
         attributeLists = topk.preProcess(vectors, evaluationFunction)
     initialTuples = topk.generateTuples(vectors, evaluationFunction, [x for x in range(len(vectors[0]))], len(vectors[0]))
     initialTopK = topk.computeTopK(initialTuples, k)
     setInitialTopK = set(initialTopK)
     for permutation in itertools.permutations(range(len(vectors[0]))):
+        currHash = 0
         for position in range(len(vectors[0])):
+            prevHash = currHash
+            currHash = currHash | (1 << permutation[position])
             if algorithm == 'Threshold':
                 topKPrev = topk.computeTopKThreshold(vectors, attributeLists, evaluationFunction, permutation[:position], k)
                 topKCurr = topk.computeTopKThreshold(vectors, attributeLists, evaluationFunction, permutation[:position+1], k)
+                setPrevTopK = set(topKPrev)
+                setCurrTopK = set(topKCurr)
+                #buggy lines, check for when sets are both empty (divide by zero)
+                IoUPrev = 0 if position == 0 else len(setInitialTopK.intersection(setPrevTopK))/len(setInitialTopK.union(setPrevTopK))
+                IoUCurr = len(setInitialTopK.intersection(setCurrTopK))/len(setInitialTopK.union(setCurrTopK))
             else:
-                prevTuples = topk.generateTuples(vectors, evaluationFunction, permutation, position)
-                currTuples = topk.generateTuples(vectors, evaluationFunction, permutation, position+1)
-                topKPrev = topk.computeTopK(prevTuples, k)
-                topKCurr = topk.computeTopK(currTuples, k)
-            setPrevTopK = set(topKPrev)
-            setCurrTopK = set(topKCurr)
-            IoUPrev = 0 if position == 0 else len(setInitialTopK.intersection(setPrevTopK))/len(setInitialTopK.union(setPrevTopK))
-            IoUCurr = len(setInitialTopK.intersection(setCurrTopK))/len(setInitialTopK.union(setCurrTopK))
-            scores[permutation[position]] = scores[permutation[position]] + (IoUCurr - IoUPrev)/math.factorial(len(vectors[0])) 
+                if prevHash not in previousSeen:
+                    prevTuples = topk.generateTuples(vectors, evaluationFunction, permutation, position)
+                    topKPrev = topk.computeTopK(prevTuples, k)
+                    setPrevTopK = set(topKPrev)
+                    IoUPrev = 0 if position == 0 else 1 if (len(setInitialTopK) == 0 and len(setPrevTopK) == 0) else len(setInitialTopK.intersection(setPrevTopK))/len(setInitialTopK.union(setPrevTopK))
+                    previousSeen[prevHash] = IoUPrev
+                else:
+                    IoUPrev = previousSeen[prevHash]
+                if currHash not in previousSeen:
+                    currTuples = topk.generateTuples(vectors, evaluationFunction, permutation, position+1)
+                    topKCurr = topk.computeTopK(currTuples, k)
+                    setCurrTopK = set(topKCurr)
+                    IoUCurr = 1 if (len(setInitialTopK) == 0 and len(setCurrTopK) == 0) else len(setInitialTopK.intersection(setCurrTopK))/len(setInitialTopK.union(setCurrTopK))
+                    previousSeen[currHash] = IoUCurr
+                else:
+                    IoUCurr = previousSeen[currHash]
+            scores[permutation[position]] = scores[permutation[position]] + (IoUCurr - IoUPrev)/dFactorial 
     return scores
 
 def ComputeWhyInTheseTopKs(vectors, evaluationFunctions, k, j, algorithm='GeneralPurpose'):
-    scores = [0 for x in range(len(vectors[0]   ))]
+    scores = [0 for x in range(len(vectors[0]))]
+    dFactorial = math.factorial(len(vectors[0]))
     if algorithm == 'Threshold':
         attributeLists = topk.preProcess(vectors)
     initialTopKs = set()
+    previousSeen = {}
     for evaluationFunction in range(len(evaluationFunctions)):
         initialTuples = topk.generateTuples(vectors, evaluationFunctions[evaluationFunction], [x for x in range(len(vectors[0]))], len(vectors[0]))
         if topk.computeInTopK(initialTuples, k, j):
             initialTopKs.add(evaluationFunction)
     for permutation in itertools.permutations(range(len(vectors[0]))):
+        currHash = 0
         for position in range(len(vectors[0])):
+            prevHash = currHash
+            currHash = currHash | (1 << permutation[position])
             prevTopKs = set()
             currTopKs = set()
-            for evaluationFunction in range(len(evaluationFunctions)):
-                if algorithm == 'Threshold':
-                    if j in topk.computeTopKThreshold(vectors, attributeLists, evaluationFunctions[evaluationFunction], permutation[:position], k):
-                        prevTopKs.add(evaluationFunction)
-                    if j in topk.computeTopKThreshold(vectors, attributeLists, evaluationFunctions[evaluationFunction], permutation[:position+1], k):
-                        currTopKs.add(evaluationFunction)
-                else:
-                    prevTuples = topk.generateTuples(vectors, evaluationFunctions[evaluationFunction], permutation, position)
-                    currTuples = topk.generateTuples(vectors, evaluationFunctions[evaluationFunction], permutation, position+1)
-                    if topk.computeInTopK(prevTuples, k, j):
-                        prevTopKs.add(evaluationFunction)
-                    if topk.computeInTopK(currTuples, k, j):
-                        currTopKs.add(evaluationFunction)
-            IoUPrev = 0 if position == 0 else 1 if (len(prevTopKs) == 0 and len(currTopKs) == 0) else len(initialTopKs.intersection(prevTopKs))/len(initialTopKs.union(prevTopKs))
-            IoUCurr = 1 if (len(prevTopKs) == 0 and len(currTopKs) == 0) else len(initialTopKs.intersection(currTopKs))/len(initialTopKs.union(currTopKs))
-            try:
-                scores[permutation[position]] = scores[permutation[position]] + (IoUCurr - IoUPrev)/math.factorial(len(vectors[0]))
-            except:
-                pdb.set_trace()
+            if True:
+                for evaluationFunction in range(len(evaluationFunctions)):
+                    if algorithm == 'Threshold':
+                        if j in topk.computeTopKThreshold(vectors, attributeLists, evaluationFunctions[evaluationFunction], permutation[:position], k):
+                            prevTopKs.add(evaluationFunction)
+                    else:
+                        prevTuples = topk.generateTuples(vectors, evaluationFunctions[evaluationFunction], permutation, position)
+                        if topk.computeInTopK(prevTuples, k, j):
+                            prevTopKs.add(evaluationFunction)
+                IoUPrev = 0 if position == 0 else 1 if (len(prevTopKs) == 0 and len(initialTopKs) == 0) else len(initialTopKs.intersection(prevTopKs))/len(initialTopKs.union(prevTopKs))
+                previousSeen[prevHash] = IoUPrev
+            else:
+                IoUPrev = previousSeen[prevHash]
+            if True:
+                for evaluationFunction in range(len(evaluationFunctions)):
+                    if algorithm == 'Threshold':
+                        if j in topk.computeTopKThreshold(vectors, attributeLists, evaluationFunctions[evaluationFunction], permutation[:position+1], k):
+                            currTopKs.add(evaluationFunction)
+                    else:
+                        currTuples = topk.generateTuples(vectors, evaluationFunctions[evaluationFunction], permutation, position+1)
+                        if topk.computeInTopK(currTuples, k, j):
+                            currTopKs.add(evaluationFunction)
+                IoUCurr = 1 if (len(initialTopKs) == 0 and len(currTopKs) == 0) else len(initialTopKs.intersection(currTopKs))/len(initialTopKs.union(currTopKs))
+                previousSeen[currHash] = IoUCurr
+            else:
+                IoUCurr = previousSeen[currHash]
+            scores[permutation[position]] = scores[permutation[position]] + (IoUCurr - IoUPrev)/dFactorial
     return scores   
     
 vectors = [[5,3,1],[2,4,4],[3,1,2],[4,1,3],[1,2,5]]
