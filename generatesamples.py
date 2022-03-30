2abee36946972578dcaa7a6334251cf29393ce1a
IanSwift <idealistictechnophilia@gmail.com>
2022-03-30 16:39:03 -0500

    Generate non-linear functions

--- generatesamples.py
+++ generatesamples.py
@@ -9,3 +9,12 @@ def generateSamples(n, d):
     for a in range(n):
         results['Functions'].append(lambda e, weights=[random.random() for x in range(d)], a=d: sum([e[x]*weights[x] for x in range(a)]))
     return results
+
+def generateNonLinearSamples(n, d):
+    results = {}
+    results['Tuples'] = [[random.random() for x in range(d)] for y in range(n)]
+    results['Tuples'] = [[x/np.linalg.norm(y, 1) for x in y] for y in results['Tuples']]
+    results['Functions'] = []
+    for a in range(n):
+        results['Functions'].append(lambda e, weights=[random.random() for x in range(d)], a=d: sum([e[x]**2*weights[x] for x in range(a)]))
+    return results
