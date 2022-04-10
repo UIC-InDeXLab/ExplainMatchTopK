928351b8048569aba96c050feefd97c0655d3923
IanSwift <idealistictechnophilia@gmail.com>
2022-04-10 12:21:17 -0500

    Update sample generation

--- generatesamples.py
+++ generatesamples.py
@@ -2,11 +2,13 @@ import random
 import numpy as np
 
 def generateSamples(n, d, p=1, zipfA=1.5):
-    results = {'Functions':[]}
+    results = {'Functions':[], 'Weights': []}
     # results['Tuples'] = [[random.random() for x in range(d)] for y in range(n)]
     # results['Tuples'] = [[x/np.linalg.norm(y, 1) for x in y] for y in results['Tuples']]
     for a in range(n):
-        results['Functions'].append(lambda e, weights=np.random.zipf(zipfA, size=(d)).astype(float).tolist(), a=d: sum([(0 if e[x] == None else (abs(e[x])**p)*weights[x]) for x in range(a)]))
+        wghts = np.random.zipf(zipfA, size=(d)).astype(float).tolist()
+        results['Functions'].append(lambda e, weights=wghts, a=d: sum([(0 if e[x] == None else (abs(e[x])**p)*weights[x]) for x in range(a)]))
+        results['Weights'].append(wghts)
     return results
 
 def generateNonLinearSamples(n, d):
