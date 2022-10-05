7cc5cc10482162fb230430a78ee71e67b5982874
Ian Swift <ian.p.swift@gmail.com>
2022-10-05 00:07:45 -0500

    only 25 samples, new dataset

--- varying_m_experiment.py
+++ varying_m_experiment.py
@@ -44,7 +44,7 @@ def findQueryPoint(tuples, k, functions, d, unWrapFunction, minim, maxim):
         maxim = maxim + 1
 
 def varyingMExperimentWhyThese(tuples, functions, reverseTuples, reverseFunctions, d, unWrapFunction, minim, maxim, k, secondUnwrap=None, secondD=None):
-    mTested = [25,50,75,100,125,150,175,200,225,250]
+    mTested = [25]
 
     results = {}
 
@@ -75,7 +75,7 @@ def findQueryPointOld(tuples, k, functions, d, unWrapFunction):
 
 def varyingMExperiment(tuples, functions, reverseTuples, reverseFunctions, d, unWrapFunction):
     k = 5
-    mTested = [25,50,75,100,125,150,175,200,225,250]
+    mTested = [25]
 
     results = {}
 
@@ -110,7 +110,7 @@ def SyntheticExperiment(methods):
     if 'AZL' in methods:
         datasets = dill.load(open('data/a_z_l_2_varying_d.dill', 'rb'))
         whyInTheseAZL = varyingMExperimentWhyThese(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None, 3, 6, 5)
-        datasets = dill.load(open('data/a_z_l_varying_d.dill', 'rb'))
+        datasets = dill.load(open('data/a_z_l_2_varying_d.dill', 'rb'))
         AZL = varyingMExperiment(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None)
         AZL['WhyInTheseTopKs'] = whyInTheseAZL['WhyInTheseTopKs']
         AZL['WhyInTheseTopKsQueryPoints'] = whyInTheseAZL['Query Points']
@@ -118,7 +118,7 @@ def SyntheticExperiment(methods):
     if 'CZL' in methods:
         datasets = dill.load(open('data/c_z_l_2_varying_d.dill', 'rb'))
         whyInTheseCZL = varyingMExperimentWhyThese(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None, 3, 6, 5)
-        datasets = dill.load(open('data/c_z_l_varying_d.dill', 'rb'))
+        datasets = dill.load(open('data/c_z_l_2_varying_d.dill', 'rb'))
         CZL = varyingMExperiment(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None)
         CZL['WhyInTheseTopKs'] = whyInTheseCZL['WhyInTheseTopKs']
         CZL['WhyInTheseTopKsQueryPoints'] = whyInTheseCZL['Query Points']
@@ -126,7 +126,7 @@ def SyntheticExperiment(methods):
     if 'IZL' in methods:
         datasets = dill.load(open('data/i_z_l_2_varying_d.dill', 'rb'))
         whyInTheseIZL = varyingMExperimentWhyThese(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None, 3, 6, 5)
-        datasets = dill.load(open('data/i_z_l_varying_d.dill', 'rb'))
+        datasets = dill.load(open('data/i_z_l_2_varying_d.dill', 'rb'))
         IZL = varyingMExperiment(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None)
         IZL['WhyInTheseTopKs'] = whyInTheseIZL['WhyInTheseTopKs']
         IZL['WhyInTheseTopKsQueryPoints'] = whyInTheseIZL['Query Points']
@@ -134,7 +134,7 @@ def SyntheticExperiment(methods):
     if 'AZNL' in methods:
         datasets = dill.load(open('data/a_z_nl_2_varying_d.dill', 'rb'))
         whyInTheseAZNL = varyingMExperimentWhyThese(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None, 3, 6, 5)
-        datasets = dill.load(open('data/a_z_nl_varying_d.dill', 'rb'))
+        datasets = dill.load(open('data/a_z_nl_2_varying_d.dill', 'rb'))
         AZNL = varyingMExperiment(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None)
         AZNL['WhyInTheseTopKs'] = whyInTheseAZNL['WhyInTheseTopKs']
         AZNL['WhyInTheseTopKsQueryPoints'] = whyInTheseAZNL['Query Points']
@@ -142,7 +142,7 @@ def SyntheticExperiment(methods):
     if 'CZNL' in methods:
         datasets = dill.load(open('data/c_z_nl_2_varying_d.dill', 'rb'))
         whyInTheseCZNL = varyingMExperimentWhyThese(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None, 3, 6, 5)
-        datasets = dill.load(open('data/c_z_nl_varying_d.dill', 'rb'))
+        datasets = dill.load(open('data/c_z_nl_2_varying_d.dill', 'rb'))
         CZNL = varyingMExperiment(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None)
         CZNL['WhyInTheseTopKs'] = whyInTheseCZNL['WhyInTheseTopKs']
         CZNL['WhyInTheseTopKsQueryPoints'] = whyInTheseCZNL['Query Points']
@@ -150,7 +150,7 @@ def SyntheticExperiment(methods):
     if 'IZNL' in methods:
         datasets = dill.load(open('data/i_z_nl_2_varying_d.dill', 'rb'))
         whyInTheseIZNL = varyingMExperimentWhyThese(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None, 3, 6, 5)
-        datasets = dill.load(open('data/i_z_nl_varying_d.dill', 'rb'))
+        datasets = dill.load(open('data/i_z_nl_2_varying_d.dill', 'rb'))
         IZNL = varyingMExperiment(datasets[9][0], datasets[9][1], datasets[9][2], datasets[9][3], 9, None)
         IZNL['WhyInTheseTopKs'] = whyInTheseIZNL['WhyInTheseTopKs']
         IZNL['WhyInTheseTopKsQueryPoints'] = whyInTheseIZNL['Query Points']
