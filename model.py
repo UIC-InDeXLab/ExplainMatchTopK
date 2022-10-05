2927638d5f1567d0a68434a3d6264a7f2d9840fd
Ian Swift <ian.p.swift@gmail.com>
2022-10-05 14:03:02 -0500

    Remove eroneous nan

--- model.py
+++ model.py
@@ -102,7 +102,7 @@ class ModelGenerator:
             inTopK = topk.computeInTopK(tuples, self.top_k, self.j)
             result.append(1 if inTopK else 0)
 
-        return np.reshape(result, len(masks, 1))nan
+        return np.reshape(result, len(masks, 1))
 
     def not_in_top_k(self, mask):
         if self.vectors is None or self.evaluationFunction is None or self.top_k is None or self.j is None:
