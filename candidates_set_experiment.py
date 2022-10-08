56732336cd6df430d7d91bc8296042cd6249ac6e
Ian Swift <ian.p.swift@gmail.com>
2022-10-08 18:13:22 -0500

    Unwrap data for candidates experiment

--- candidates_set_experiment.py
+++ candidates_set_experiment.py
@@ -57,10 +57,10 @@ def varyingMExperimentCandidates(tuples, functions, reverseTuples, reverseFuncti
     notInTopKModel.database(tuples).eval_func(functions[borderlineFunc]).k(k).unwrap_func(unWrapFunction).target(t)
 
     whyThisTopKModel = ModelGenerator()
-    whyThisTopKModel.database(reverseTuples).eval_func(reverseFunctions[t]).k(k).unWrapFunction(secondUnwrap).setup_top_k()
+    whyThisTopKModel.database(reverseTuples).eval_func(reverseFunctions[t]).k(k).unwrap_func(secondUnwrap).setup_top_k()
 
     whyInTheseTopKModel = ModelGenerator()
-    whyInTheseTopKModel.database(tuples).eval_funcs(functions).k(k).target(t).unWrapFunction(unWrapFunction).setup_top_ks()
+    whyInTheseTopKModel.database(tuples).eval_funcs(functions).k(k).target(t).unwrap_func(unWrapFunction).setup_top_ks()
 
     for m in mTested:
         inTopKResults['Approximate'][m] = experiments.approximateInTopK(tuples, functions[topkFunc], m, k, t, d, inTopKResults['BruteForce']['ShapleyValues'], unWrapFunction)
@@ -84,7 +84,7 @@ def CandidatesExperiment():
     datasets = dill.load(open('Candidates-Dataset.dill', 'rb'))
     functions = dill.load(open('Revised-Candidate-Functions.dill', 'rb'))
     dill.dump(varyingMExperimentCandidates(datasets['Candidates'], functions['HRs'], datasets['HRs'], functions['Candidates'], 9,
-                                 UnwrapCandidate, 3, 6, 5, secondUnwrap=UnwrapHR, secondD=8), open('VaryingMCandidates-Clean.dill', 'wb'))
+                                 UnwrapCandidate, 3, 6, 5, secondUnwrap=UnwrapHR, secondD=8), open('VaryingMCandidates-Clean    .dill', 'wb'))
 
 
 if __name__ == "__main__":
